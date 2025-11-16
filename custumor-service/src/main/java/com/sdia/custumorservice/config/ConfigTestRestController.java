222630f0fef2a87b780e3a3713b13ab1080cf67e
ABOUCHELAILA <TON_EMAIL@exemple.com>
2025-11-16 15:12:03 +0100

    configuration et test1

--- custumor-service/src/main/java/com/sdia/custumorservice/config/ConfigTestRestController.java
+++ custumor-service/src/main/java/com/sdia/custumorservice/config/ConfigTestRestController.java
@@ -1,6 +1,27 @@
 package com.sdia.custumorservice.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+
+// optionnel, mais si tu l’as, il change le chemin
 public class ConfigTestRestController {
 
+    @Value("${global.params.p1}")
+    private String p1;
 
+    @Value("${global.params.p2}")
+    private String p2;
+
+    @GetMapping("/testConfig1")
+    public Map<String, String> configTest() {
+        return Map.of("p1", p1, "p2", p2);
+    }
 }
+
